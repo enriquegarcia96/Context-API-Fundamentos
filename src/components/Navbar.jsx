b4d9f33a62936b933c647e66b908898c24d77c99
enriquegarcia96 <enriquegarcia259@yahoo.es>
2021-04-16 21:49:03 -0600

    creando el useContext al componente

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+import { ThemeContext } from '../context/ThemeProvider';
+
+
+const Navbar = () => {
+
+    const {theme, cambioColor} = React.useContext(ThemeContext); // este es un HOOK
+
+    return (
+        <div style={
+            {
+                background: theme.background,
+                color: theme.color
+            }
+        }>
+            <h1>Navbar</h1>
+            <label>Color Texto</label>
+            <input 
+                type="color"
+                onChange={ e => cambioColor({...theme, color: e.target.value }) }
+            />
+
+            <label>Color Fondo</label>
+            <input 
+                type="color"
+                onChange={ e => cambioColor({ ...theme, background: e.target.value }) }
+            />
+
+        </div>
+    )
+}
+
+
+export default Navbar
